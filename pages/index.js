e8e6cd21f2deadbdd1e68a9a2fd631a25885b48d
adittya07 <adittyams@gmail.com>
2021-09-11 20:51:16 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,7 @@ import Loader from '../components/Loader'
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <div>
-        <Loader show />
-      </div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: {username:'ak123'}
-      }}>
-        <a>Arshad oru playgod</a>
-      </Link>
-=======
       <h1>Hello AK</h1>
->>>>>>> df998dc25b547da4712565d18278a0f91f0cd7eb
     </div>
   )
 }
