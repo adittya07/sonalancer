3d18877653be003b50847e7a1a5f8b2378034ba5
adittya07 <88475775+adittya07@users.noreply.github.com>
2021-11-04 23:06:08 +0530

    Create St form as app.js

---
+++ St form as app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+//import Background from './components/Form';
+
+function App() {
+  return (
+
+    <div className="App">
+      <Form />
+  
+    </div>
+
+  );
+}
+
+export default App;
